a907d9be9345fa8bde42c05255a6a90fe50e1ec9
Enguerrand <engue0774@gmail.com>
2017-11-14 22:09:36 +0100

    ModuleAPI(feat): Add IConfigKey (Set/Get: Type, Name, Value)

--- src/ConfigKey.hpp
+++ src/ConfigKey.hpp
@@ -11,11 +11,54 @@
 #ifndef CPP_ZIA_API_CONFIGKEY_HPP
 #define CPP_ZIA_API_CONFIGKEY_HPP
 
+#include <string>
+
 namespace ziaAPINexus {
 	class IConfigKey {
+	  enum class Type {
+		VALUE	= 0,
+		SCOPE	= 1
+	  };
+
 	  virtual ~IConfigKey() = default;
 
-	  virtual
+	  /**
+	   * Set type of config key
+	   * @param type
+	   */
+	  virtual void setType(const Type & type) = 0;
+
+	  /**
+	   * Get type of config key
+	   * @return
+	   */
+	  virtual const Type & getType(void) const = 0;
+
+	  /**
+	   * Set name of config key
+	   * @param type
+	   */
+	  virtual void setName(const std::string & name) = 0;
+
+	  /**
+	   * Get name of config key
+	   * @return
+	   */
+	  virtual const std::string & getName(void) const = 0;
+
+	  /**
+	   * Set value of config key
+	   * @attention Not for scope type
+	   * @param type
+	   */
+	  virtual void setValue(const std::string & value) = 0;
+
+	  /**
+	   * Get value of config key
+	   * @attention Return "" for scope
+	   * @return
+	   */
+	  virtual const std::string & getValue(void) const = 0;
 	};
 }
 
