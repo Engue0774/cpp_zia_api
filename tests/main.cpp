40416ebdde026fcb776d806f7c769ead2e18d2af
Enguerrand <engue0774@gmail.com>
2017-11-15 10:43:20 +0100

    BuildCheck(feat): Add travis build and main for test header

---
+++ tests/main.cpp
@@ -0,0 +1,16 @@
+/*
+** main.cpp for cpp_zia_api in /home/enguerrand/delivery/cpp_zia_api/main.cpp
+**
+** Made by Enguerrand Allamel
+** Login   <enguerrand.allamel@epitech.eu>
+**
+** Started on  Wed Nov 15 09:49:27 2017 Enguerrand Allamel
+** Last update Wed Nov 15 09:49:27 2017 Enguerrand Allamel
+*/
+
+#include "../src/NexusZiaAPI.hpp"
+
+int main(int, char**)
+{
+  return (0);
+}
