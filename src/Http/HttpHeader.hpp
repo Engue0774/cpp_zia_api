b41cf2bb826008cbc0ff20c170392cfcba16e5cb
Timothy Diguiet <timothy.diguiet@epitech.eu>
2017-12-11 15:43:09 +0100

    Update HttpHeader.hpp

--- src/Http/HttpHeader.hpp
+++ src/Http/HttpHeader.hpp
@@ -29,7 +29,7 @@ class IHttpHeader {
    * @param key
    * @return
    */
-  virtual const std::string & getKey(const std::string & key) = 0;
+  virtual const std::string & getKey(const std::string & key) const = 0;
 };
 }
 
